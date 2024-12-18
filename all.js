b4c1efc8200af8eb8ac64a9a825dad0f4f8963d5
Chao Yu <ae24856@gmail.com>
2024-12-18 14:00:26 +0800

    Add CSS transition and answer items 6 & 7

--- all.js
+++ all.js
@@ -1,20 +1,21 @@
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("dialog + button");
-const closeButton = document.querySelector("dialog button");
+const dialog = document.querySelector('dialog');
+const showButton = document.querySelector('dialog + button');
+const closeButton = document.querySelector('dialog button');
 
 // "Show the dialog" button opens the dialog modally
-showButton.addEventListener("click", () => {
+showButton.addEventListener('click', () => {
   dialog.showModal();
 });
 
 // "Close" button closes the dialog
-closeButton.addEventListener("click", () => {
+closeButton.addEventListener('click', () => {
   dialog.close();
 });
 
 // 點擊空白處時關閉 dialog
-dialog.addEventListener("click", (e) => {
-  if (e.target === dialog) { // 判斷是否點擊到 <dialog> 本身（背景區域）
+dialog.addEventListener('click', (e) => {
+  if (e.target === dialog) {
+    // 判斷是否點擊到 <dialog> 本身（背景區域）
     dialog.close();
   }
 });
